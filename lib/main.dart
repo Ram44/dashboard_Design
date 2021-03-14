7a705c2d3a767c91e149d08388d0e3fcce79046d
Ram Prasad Sharma <ram132527@gmail.com>
2021-03-14 15:12:32 +0545

    box decoration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:app/myHomePage.dart';
 import 'package:app/screen/dashboard.dart';
+import 'package:app/screen/page/my_pages.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: StackDemo(),
+    home: MyPages(),
     debugShowCheckedModeBanner: false,
   ));
 }
